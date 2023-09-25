43d0e6240baf45ebebdd4efd930c39c12eec9d86
root <root@test0.auvtel.net>
2023-09-25 16:41:09 +0000

    many corrections

---
+++ scripts/nginx.sh
@@ -0,0 +1,48 @@
+# params 
+# 1 : email for LetsEncrypt
+# 2 : hostname use for base url
+
+
+if [ -z "$1" ];then 
+  echo "email for Letsencrypt TOS is missing"
+
+  exit
+fi
+
+email=$1
+
+if [ -z "$2" ];then 
+  hostname=$(hostname)
+else
+  hostname=$2
+fi
+
+php_default_version=7.4
+
+export DEBIAN_FRONTEND=noninteractive
+
+apt update -y
+apt dist-upgrade -y
+apt install -y bash-completion curl nano
+
+bash ../modules/module/wrappers/install
+
+tb app sudo/install/base
+tb app sudo/install/mariadb
+tb app sudo/install/php 7.4
+tb app sudo/install/php 8.1
+tb app sudo/install/web --nginx
+tb app sudo/install/ssl $hostname --nginx
+tb app sudo/way/init nginx 7.4 $email $hostname $php_default_version
+tb app sudo/install/files
+service nginx restart
+service php7.4-fpm restart
+service php8.1-fpm restart
+service mariadb restart
+
+echo "# MARIADB"
+echo "# /root/.my.cnf"
+cat /root/.my.cnf
+
+echo "# Install phpMyAdmin"
+echo tb app sudo/create pma-v1 --certbot --template_install=pma
---
+++ scripts/proxy.sh
@@ -0,0 +1,37 @@
+# params
+# 1 : email for LetsEncrypt
+# 2 : hostname use for base url
+
+if [ -z "$1" ]; then
+  echo "email for Letsencrypt TOS is missing"
+
+  exit
+fi
+
+email=$1
+
+if [ -z "$2" ]; then
+  hostname=$(hostname)
+else
+  hostname=$2
+fi
+
+export DEBIAN_FRONTEND=noninteractive
+
+apt update -y
+apt dist-upgrade -y
+apt install -y bash-completion curl nano
+
+bash ../modules/module/wrappers/install
+
+tb app sudo/install/base
+tb app sudo/install/web --proxy
+tb app sudo/install/ssl $hostname --proxy
+tb app sudo/way/init nginx $email $hostname
+tb app sudo/install/files
+service nginx restart
+service php7.4-fpm restart
+service php8.1-fpm restart
+service mariadb restart
+
+echo "# tb app sudo/way/proxy/create test-v1 http://127.0.0.1/ --certbot"
